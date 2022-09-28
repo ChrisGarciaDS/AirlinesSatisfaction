060dc0019913d5c324307323a27aaea95979efd4
Christopher Garcia <93369045+ChrisGarciaDS@users.noreply.github.com>
2022-09-27 17:30:34 -0700

    Add data mining file

---
+++ AirlineDataMining.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Airlines Customer Satisfaction
+# 
+# **Team 4**: Christopher Garcia, Dennis	Myasnyankin, Grigor	Tashchyan 
+# 
+# **Date**: October 17, 2022
+# 
+# Objective? Maybe skip this
+
+# In[4]:
+
+
+# Testing installs
+get_ipython().system('pip install dmba')
+
+
+# In[6]:
+
+
+# Load required packages/libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+get_ipython().run_line_magic('matplotlib', 'inline')
+plt.style.use('ggplot')
+from dmba import liftChart
+
+
+# In[ ]:
+
+
+# Load and import both training and test sets
+train_data = ''
+test_data = ''
+
